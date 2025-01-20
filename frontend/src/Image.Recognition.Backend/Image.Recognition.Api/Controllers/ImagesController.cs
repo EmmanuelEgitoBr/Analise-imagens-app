0508f71db08fc35f69bfaf6f712466ec4ac71b3f
Stanislaw Cruz <techway.stanislaw@odontosystem.com.br>
2025-01-20 17:39:11 -0300

    Criacao da Api

---
+++ frontend/src/Image.Recognition.Backend/Image.Recognition.Api/Controllers/ImagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Image.Recognition.Api.Controllers
+{
+    [Route("api/images")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("save-image")]
+        public IActionResult SaveImage()
+        {
+            return Ok();
+        }
+    }
+}
