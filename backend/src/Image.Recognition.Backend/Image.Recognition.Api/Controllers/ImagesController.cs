1b55e6224ea619f81d291d6b221bff96ed17d8e0
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-01-22 01:05:15 -0300

    Montagem da estrutura da Api

---
+++ backend/src/Image.Recognition.Backend/Image.Recognition.Api/Controllers/ImagesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Image.Recognition.Api.Controllers
+{
+    [Route("api/v1/images")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("/save-mongo")]
+        public IActionResult SaveImageInMongoDb(IFormFile file)
+        {
+            return Ok(file.FileName);
+        }
+
+        [HttpPost("/save-bucket")]
+        public IActionResult SaveImageInS3Bucket(IFormFile file)
+        {
+            return Ok(file.FileName);
+        }
+    }
+}
