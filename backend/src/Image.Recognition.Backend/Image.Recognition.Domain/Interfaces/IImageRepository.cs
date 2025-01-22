702750f1e7cd405e4384c1460b26acb8664fada7
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-01-22 02:36:36 -0300

    Apagando estruturas desnecessarias da api

--- backend/src/Image.Recognition.Backend/Image.Recognition.Domain/Interfaces/IImageRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Image.Recognition.Domain.Interfaces
-{
-    public interface IImageRepository
-    {
-        Task SaveImageAsync(IFormFile file);
-    }
-}
